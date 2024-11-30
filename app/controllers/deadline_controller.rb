ba3de8c7830e5f4c28647b0406a0fa4c2d93683c
Rinsei Saito <rinsei3110@gmail.com>
2024-11-30 18:01:43 +0900

    TopPageで残り日数を表示できるようにした

--- app/controllers/deadline_controller.rb
+++ app/controllers/deadline_controller.rb
@@ -3,10 +3,28 @@ class DeadlineController < ApplicationController
     @deadline = Deadline.new
   end
 
+  def show
+    @deadline = Deadline.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to deadlines_path, alert: "Deadline not found"
+  end
+
   def create
-    @deadline = Deadline.new(deadline_params)
+    # 送信された値から日付を作成
+    user_year = params[:deadline][:user_year].to_i
+    user_month = params[:deadline][:user_month].to_i
+    user_date = params[:deadline][:user_date].to_i
+    user_hour = params[:deadline][:user_hour].to_i
+    user_min = params[:deadline][:user_min].to_i
+    user_sec = params[:deadline][:user_sec].to_i
+
+    due_date = DateTime.new(user_year, user_month, user_date, user_hour, user_min, user_sec)
+
+    # 新しいdeadlineを作成して保存
+    @deadline = Deadline.new(due_date: due_date, user_id: current_user.id)
+
     if @deadline.save
-      redirect_to @deadline, notice: "Deadline was successfully created."
+      redirect_to @deadline
     else
       render :new
     end
@@ -18,4 +36,3 @@ class DeadlineController < ApplicationController
     params.require(:deadline).permit(:user_id, :content, :penalty, :due_date)
   end
 end
-
--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -2,9 +2,16 @@ class MypageController < ApplicationController
   before_action :authenticate_user!
   def index
     @user = current_user
+    @deadline = Deadline.find_by(user_id: @user.id)
+    @remaining_days = calculate_remaining_days(@deadline) if @deadline.present?
   end
 
+  private
 
+  def calculate_remaining_days(deadline)
+    # 現在の日付と目標日付を比較して残り日数を計算
+    (deadline.due_date.to_date - Date.today).to_i
+  end  
   def link
   end
 end
