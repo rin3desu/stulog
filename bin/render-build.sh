de1ce8e1ee4c489c9dbe91c16799c46e2f5b2316
rin3desu <rinsei3110@gmail.com>
2026-03-12 18:58:04 +0900

    renderのシェルファイルを変更

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,6 +4,10 @@ set -o errexit
 
 bundle install
 yarn install
+
+# ↓↓↓ この1行を追加します！ ↓↓↓
+yarn add @babel/plugin-proposal-private-methods @babel/plugin-proposal-class-properties @babel/plugin-proposal-private-property-in-object
+
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
